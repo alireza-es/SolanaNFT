83384edd3092454f30865ceb16324c14e054c819
Alireza <alireza.es@gmail.com>
2022-01-07 17:50:57 +0330

    Generate new target wallet

--- index.js
+++ index.js
@@ -32,4 +32,29 @@ const splToken = require('@solana/spl-token');
   const fromTokenAccount = await mint.getOrCreateAssociatedAccountInfo(
     fromWallet.publicKey
   );
+
+  // Generate a new wallet to receive newly minted tokens
+  const toWallet = web3.Keypair.generate();
+
+  // Get the token account of the toWallet Solana address, if it doesn't exist, create it
+  const toTokenAccount = await mint.getOrCreateAssociatedAccountInfo(
+    toWallet.publicKey
+  );
+
+  // Mint 1 new token to the fromTokenAccount we just returned or created
+  await mint.mintTo(
+    fromTokenAccount.address, // Who it goes to
+    fromWallet.publicKey, // Minting authority
+    [], // Multisig signers
+    1000000000 // Amount of tokens to mint
+  );
+
+  // Set Authority to the toWallet Solana address
+  await mint.setAuthority(
+    mint.publicKey,
+    null,
+    "MintTokens",
+    fromWallet.publicKey,
+    []
+  );
 })
