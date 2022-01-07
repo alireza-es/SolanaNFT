74c150dbd4faefec7cf8a8c2004efcc30e8793af
Alireza <alireza.es@gmail.com>
2022-01-07 17:49:40 +0330

    Generate source wallet

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const splToken = require('@solana/spl-token');
 (async () => {
   // Connect to cluster
   const connection = new web3.Connection(
-    web3.clusterApiUrl('devnet'),
-    'confirmed',
+    web3.clusterApiUrl("devnet"),
+    "confirmed"
   );
+
+  // Generate the source wallet
+  const fromWallet = web3.Keypair.generate();
+  const fromAirDropSignature = await connection.requestAirdrop(
+    fromWallet.publicKey,
+    web3.LAMPORTS_PER_SOL
+  );
+  
 })
